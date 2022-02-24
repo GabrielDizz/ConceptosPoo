4a623903e7b140643eda0b1da4b1f6ad4b68be89
Gabrieldizz <gdizzett.u@gmail.com>
2022-02-24 17:57:56 -0500

    Finish DateClass

---
+++ ConceptosPoo/ConceptosPoo/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConceptosPoo;
+
+Console.WriteLine("Conceptos Poo");
+Console.WriteLine("==============");
+
+Date date1 = new Date(2024,2,29);
+Date date2 = new Date(2022,5,30);
+try
+{
+    Console.WriteLine(date1);
+    Console.WriteLine(date2);
+}
+catch (Exception error)
+{
+  Console.WriteLine(error.Message);
+}
